0dd1d72e71e1be8c09053b6e5a3c53aef6442e94
Efrain <carlosefrainchallo1@gmail.com>
2025-05-23 16:35:00 -0500

    Se agrego la estructura de los modelos.

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['post_id', 'file_path', 'file_type'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
