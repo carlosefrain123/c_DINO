0b3a8ea62bf8bb625dff2fc077fe6e7c3a00e4ff
Efrain <carlosefrainchallo1@gmail.com>
2025-05-23 15:23:43 -0500

    Se agrega en la migración users, los roles que corresponden.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-             $table->string('phone', 20)->nullable();
-            $table->enum('role', ['admin', 'customer'])->default('customer');
+            $table->string('phone', 20)->nullable();
+            $table->enum('role', ['admin', 'customer', 'editor'])->default('customer');
             $table->text('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
