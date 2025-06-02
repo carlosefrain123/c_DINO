bef926589dc7528b21e159c890b81fd5a39cc395
Efrain <carlosefrainchallo1@gmail.com>
2025-06-02 02:32:58 -0500

    Se agrega la interfaz de buscar la cual busca por tags y categorias. Te redirige a la interfaz de blod

--- resources/views/home/buscar.blade.php
+++ resources/views/home/buscar.blade.php
@@ -1,3 +1,19 @@
 <div class="search-box input-group">
-    <input type="search" class="form-control" placeholder="Estoy buscando...">
+    <form method="GET" action="{{ route('blog.index') }}" class="d-flex w-100">
+        <input
+            type="search"
+            name="search"
+            class="form-control"
+            placeholder="Buscar en
+                @if(request()->routeIs('posts.category'))
+                    {{ $category->name ?? 'el blog' }}
+                @elseif(request()->routeIs('posts.tag'))
+                    {{ $tag->name ?? 'el blog' }}
+                @else
+                    el blog
+                @endif"
+            value="{{ request('search') }}"
+        >
+        <button type="submit" class="btn btn-primary">Buscar</button>
+    </form>
 </div>
