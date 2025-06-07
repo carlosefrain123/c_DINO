5a4ebda4d880cb4b32c2c6be0270d68398e6de92
Efrain <carlosefrainchallo1@gmail.com>
2025-06-07 16:55:40 -0500

    Modificamos el proyecto en el banner, que solo muestre los banners, cabe mencioanr que el 'slug' tiene que tener el mismo nombre que categories

--- resources/views/home/carousel.blade.php
+++ resources/views/home/carousel.blade.php
@@ -6,21 +6,22 @@
                 <div class="slider-animate">
                     {{-- El latestPosts es el carrusel, no te olvides. No contiene route --}}
                     @foreach ($latestPosts as $post)
-                        <div>
-                            <div class="home-contain rounded-0 p-0">
-                                <img src="{{ asset('storage/' . $post->featured_image) }}"
-                                    class="img-fluid bg-img blur-up lazyload bg-top" alt="{{ $post->title }}">
-                                <div class="home-detail p-center text-center home-overlay position-relative">
-                                    <div class="content">
-                                        <h1>{{ $post->title }}</h1>
-                                        <h3>{{ $post->summary }}</h3>
-                                        @include('home.buscar')
+                        @if ($post->categories->contains('slug', 'banner'))
+                            <div>
+                                <div class="home-contain rounded-0 p-0">
+                                    <img src="{{ asset('storage/' . $post->featured_image) }}"
+                                        class="img-fluid bg-img blur-up lazyload bg-top" alt="{{ $post->title }}">
+                                    <div class="home-detail p-center text-center home-overlay position-relative">
+                                        <div class="content">
+                                            <h1>{{ $post->title }}</h1>
+                                            <h3>{{ $post->summary }}</h3>
+                                            @include('home.buscar')
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        @endif
                     @endforeach
-
                 </div>
             </div>
         </div>
